60792bb890603380fcc26bba011c2f9e30c760bb
Jmart5564 <Jmart5564@gmail.com>
2022-05-31 15:27:54 -0700

    adding some css and html

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 // grab DOM elements
-
+const nameInput = document.getElementById('name-input');
+const nameDisplay = document.getElementById('name-display');
 // set event listeners
+nameInput.addEventListener('input', () => {
+const name = nameInput.value;
+    console.log('Success!');
+    nameDisplay.textContent = name;
+})
     // get info from user input
     // use user input to update state
     // update DOM to reflect the new state
