0e8763cd2c8cb3c706c3fc42cbb6bcfe107cb2b4
Jmart5564 <Jmart5564@gmail.com>
2022-06-01 10:50:50 -0700

    Final additions

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ nameInput.addEventListener('input', () => {
     //console.log('Success!');
     const name = nameInput.value;
     nameDisplay.textContent = name;
-})
+});
 
 const freetimeInput = document.getElementById('freetime-input');
 const freetimeDisplay = document.getElementById('freetime-display');
  //set event listeners
 freetimeInput.addEventListener('input', () => {
-    //console.log('Success!');
+    //console.log('Success!!');
     const freetime = freetimeInput.value;
     freetimeDisplay.textContent = freetime;
-})
+});
 // get info from user input
 // use user input to update state
 // update DOM to reflect the new state
