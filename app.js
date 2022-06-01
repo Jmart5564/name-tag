12021ce51cecd2930c19aff06a1b9039abe9282b
Jmart5564 <Jmart5564@gmail.com>
2022-05-31 21:18:43 -0700

    adding gradients and css

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const nameInput = document.getElementById('name-input');
 const nameDisplay = document.getElementById('name-display');
 // set event listeners
 nameInput.addEventListener('input', () => {
-const name = nameInput.value;
-    console.log('Success!');
+    //console.log('Success!');
+    const name = nameInput.value;
     nameDisplay.textContent = name;
 })
-    // get info from user input
-    // use user input to update state
-    // update DOM to reflect the new state
+
+// get info from user input
+// use user input to update state
+// update DOM to reflect the new state
