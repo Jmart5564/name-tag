61af7d3a8a99ce20b5433081f0e813cc6d40cf47
Jmart5564 <Jmart5564@gmail.com>
2022-06-22 16:39:49 -0700

    added state and events as well as validation

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const nameInput = document.getElementById('name-input');
 const nameDisplay = document.getElementById('name-display');
 // set event listeners
 nameInput.addEventListener('input', () => {
-    //console.log('Success!');
     const name = nameInput.value;
+    console.log(name);
     nameDisplay.textContent = name;
+    console.log(name);
 });
 
 const freetimeInput = document.getElementById('freetime-input');
 const freetimeDisplay = document.getElementById('freetime-display');
  //set event listeners
 freetimeInput.addEventListener('input', () => {
-    //console.log('Success!!');
     const freetime = freetimeInput.value;
     freetimeDisplay.textContent = freetime;
 });
